d43b2890b5b6bf5d6a2adc46fcf98a8ced22cf46
torbuloloy <ljcorcullo00102@usep.edu.ph>
2026-02-10 11:21:54 +0800

    Add CategoryFactory and update routes, seeder, and welcome view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/second',[CategoryController::class, 'index'])->name('second');
+
+//Route::view('/second', 'second');
